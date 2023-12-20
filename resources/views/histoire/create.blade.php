9898d87e789a846d71952554b4adc55dae9226ca
Roméo Jacquemin <romeo_jacquemin@ens.univ-artois.fr>
2023-12-20 17:54:39 +0100

    close #9 - page de création d'une histoire

--- resources/views/histoire/create.blade.php
+++ resources/views/histoire/create.blade.php
@@ -14,26 +14,26 @@
     <form action="{{route('histoires.store')}}" method="POST">
         {!! csrf_field() !!}
         <div class="text-center" style="margin-top: 2rem">
-            <h3>Création d'un commentaire</h3>
+            <h3>Création d'une histoire</h3>
             <hr class="mt-2 mb-2">
         </div>
         <div>
-            <label for="user_id"><strong>votre id : </strong></label>
-            <input type="text" name="user_id" id="user_id">
+            <input type="text" name="titre" id="titre" placeholder="Titre">
         </div>
         <div>
-            <label for="scene_id"><strong>Votre commentaire : </strong></label>
-            <input type="text" class="form-control" id="scene_id" name="scene_id">
+            <input type="file" name="media" id="doc">
         </div>
         <div>
-            <label for="titre"><strong>titre du commentaire : </strong></label>
-            <input type="text" name="titre" id="titre"
-                   value="{{ old('titre') }}">
+            <select name="genre_id" id="genre_id">
+                <option value="1">SF</option>
+                <option value="2">Comics</option>
+                <option value="3">Policier</option>
+                <option value="4">Drame</option>
+                <option value="5">Comédie</option>
+            </select>
         </div>
         <div>
-            <label for="commentaire"><strong>Votre commentaire : </strong></label>
-            <input type="text" class="form-control" id="commentaire" name="commentaire"
-                   value="{{ old('commentaire') }}">
+            <textarea type="" class="form-control" id="pitch" name="pitch"> </textarea>
         </div>
         <div>
             <button class="btn btn-success" type="submit">Valide</button>
