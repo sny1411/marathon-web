b65c57bf955e61d42aec1cc73ed665d40bdd13e2
Mattéo Humez <matteo_humez@ens.univ-artois.fr>
2023-12-20 17:30:15 +0100

    :fire: #21 correction route index

--- routes/web.php
+++ routes/web.php
@@ -19,29 +19,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function (Request $request) {
-    $cat = $request->input('cat', null);
-    $value = $request->cookie('cat', null);
-
-    if (!isset($cat)) {
-        if (!isset($value)) {
-            $histoires = Histoire::inRandomOrder()->get();
-            $cat = 'All';
-            Cookie::expire('cat');
-        } else {
-            $histoires = Histoire::where('genre_id', $value)->get();
-            $cat = $value;
-            Cookie::queue('cat', $cat, 10);            }
-    } else {
-        if ($cat == 'All') {
-            $histoires = Histoire::inRandomOrder()->get();
-            Cookie::expire('cat');
-        } else {
-            $histoires = Histoire::where('genre_id', $cat)->get();
-            Cookie::queue('cat', $cat, 10);
-        }
-    }
-    $genres = \App\Models\Genre::distinct()->pluck("id");
-    return view('accueil', ['histoires' => $histoires, 'cat' => $cat, 'genres' => $genres]);
+    return redirect()->route('histoires.index');
 })->name("index");
 
 Route::get('/contact', function () {
