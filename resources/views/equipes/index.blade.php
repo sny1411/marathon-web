15b2742939b6d97587ead9b07d61788bd2f90cd5
Félix Silvert <felix_silvert@ens.univ-artois.fr>
2023-12-20 12:40:33 +0100

    close #1 Réalisation du Controller, de la page et de la route pour l'équipe

---
+++ resources/views/equipes/index.blade.php
@@ -0,0 +1,21 @@
+@extends("templates.app")
+
+@section('content')
+    <div style="display: flex;align-items: center; justify-content: center">
+        <tr>
+            <td >{{$equipe['nomEquipe']}}</td>
+            <td ><img src="{{Storage::url($equipe['logo'])}}" alt="logo" width="50" height="60"></td>
+            <td >{{$equipe['slogan']}}</td>
+            <td >{{$equipe['localisation']}}</td>
+            @foreach($equipe['membres'] as $membre)
+                <td >{{$membre['nom']}}</td>
+                <td >{{$membre['prenom']}}</td>
+                <td ><img src="{{Storage::url($membre['image'])}}" alt="pdp" width="50" height="60"> </td>
+                @foreach($membre['fonctions'] as $fonction)
+                    <td >{{$fonction}}</td>
+                @endforeach
+            @endforeach
+            <td >{{$equipe['autres']}}</td>
+        </tr>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EquipeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,4 +25,7 @@ Route::get('/contact', function () {
 Route::get('/test-vite', function () {
     return view('test-vite');
 })->name("test-vite");
+
+Route::get('/equipe', [EquipeController::class, 'index']);
+
 
