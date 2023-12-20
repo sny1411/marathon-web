73838e463d782a483df4eb0d5095d1e45af9e76a
Mattéo Humez <matteo_humez@ens.univ-artois.fr>
2023-12-20 13:45:54 +0100

    :sparkles: close #5 ajout du filtrage par genre dans la vue accueil

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -1,6 +1,20 @@
 @extends("templates.app")
 
 @section('content')
+
+    <h4>filtrage par genre :</h4>
+    <form action="{{route('index')}}" method="get">
+        <label>
+            <select name="cat">
+                <option value="All" @if($cat == 'All') selected @endif>-- Toutes catégories --</option>
+                @foreach($genres as $genre)
+                    <option value="{{$genre}}" @if($cat == $genre) selected @endif>{{ \App\Models\Genre::find($genre)->label }}</option>
+                @endforeach
+            </select>
+        </label>
+        <input type="submit" value="OK">
+    </form>
+
     @foreach($histoires as $histoire)
         <div>
             <div>{{ $histoire->titre }}</div>
