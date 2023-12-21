d1562512314f771e5a07865904bc4e75fe2691ac
Mattéo Humez <matteo_humez@ens.univ-artois.fr>
2023-12-21 05:03:30 +0100

    :bug: problèmes dans chapitre #26

--- resources/views/chapitres/create.blade.php
+++ resources/views/chapitres/create.blade.php
@@ -1,22 +1,66 @@
 @extends("templates.app")
 
 @section('content')
+    <form action="{{route("chapitre.store")}}" method="POST">
+        {!! csrf_field() !!}
+        <div>
+            <label for="titre">Titre :</label>
+            <input type="text" id="titre" name="titre"
+                   value="{{ old('titre') }}">
+        </div>
+
+        <div>
+            <label for="titreCourt">Titre Court:</label>
+            <input type="text" id="titreCourt" name="titreCourt"
+                   value="{{ old('titreCourt') }}">
+        </div>
+
+        <div>
+            <label for="document">Media :</label>
+            <input type="file" id="document" name="document">
+        </div>
+
+        <div>
+            <label for="question">Question :</label>
+            <input type="text" id="question" name="question"
+                   value="{{ old('question') }}">
+        </div>
+
+        <div>
+            <label for="premierTexte">Premier texte</label>
+            <input type="checkbox" id="premierTexte" name="premierTexte" @if(old('premierTexte')) checked @endif>
+        </div>
+
+        <div style="display: none;">
+            <label for="histoire">histoire:</label>
+            <input type="text" id="histoire" name="histoire"
+                   value="{{$histoire->id}}">
+        </div>
+
+        <label for="texte"></label><textarea id="texte" name="texte"></textarea>
+
+        <button type="submit">Envoyer</button>
+    </form>
+
     <form action="{{route('chapitre.liaison')}}" method="POST">
+        {!! csrf_field() !!}
         <h1>Liaison des chapitres</h1>
         <label for="source">Source :</label>
         <select name="source">
-            @foreach($histoire->chapitre as $chapitre)
+            @foreach($histoire->chapitres as $chapitre)
                 <option value="{{$chapitre->id}}">{{$chapitre->id}}</option>
             @endforeach
         </select>
         <label for="destination">Destination :</label>
         <select name="destination">
-            @foreach($histoire->chapitre as $chapitre)
+            @foreach($histoire->chapitres as $chapitre)
                 <option value="{{$chapitre->id}}">{{$chapitre->id}}</option>
             @endforeach
         </select>
         <div>
             <input type="text" name="reponse" placeholder="Réponse">
         </div>
+
+        <button type="submit">Envoyer</button>
     </form>
 @endsection
