d672dcf0b3b1a9b7c6c9a4b1c3a9a9c605bbb0f1
Roméo Jacquemin <romeo_jacquemin@ens.univ-artois.fr>
2023-12-21 17:04:29 +0100

    correction code

--- resources/views/templates/app.blade.php
+++ resources/views/templates/app.blade.php
@@ -43,7 +43,7 @@
                     <span class="line line3"></span>
                 </div>
                 <div class="logo">
-                    <img src="{{Vite::asset('/resources/images/logoBookShelf.png')}}" alt="pdp" class="logo">
+                    <img src="{{Vite::asset('resources/images/logoBookShelf.png')}}" alt="pdp" class="logo">
                 </div>
                 <div class="menu-items">
                     <li><a href="{{route('index')}}">Accueil</a></li>
